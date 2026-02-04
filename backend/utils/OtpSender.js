424069e642003a836cbdaf774d67424c3433f459
Sellamuthu2007 <sellamuthur.cse2024@citchennai.net>
2026-02-04 13:23:44 +0530

    Added email-otp sender

--- backend/utils/OtpSender.js
+++ backend/utils/OtpSender.js
@@ -3,8 +3,8 @@ import nodemailer from 'nodemailer';
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: process.env.EMAIL_USER, // must be 'user', not 'email'
-        pass: process.env.EMAIL_PASS, // must be 'pass', not 'pwd'
+        user: process.env.EMAIL_USER, 
+        pass: process.env.EMAIL_PASS, 
     }
 });
 
