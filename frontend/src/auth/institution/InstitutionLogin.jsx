2e129cb1267f8041f205710e6b0a0d090a33343c
senthamizhselvan-sm <senthamizhselvan.sm@gmail.com>
2026-02-06 11:53:39 +0530

    Reorder import statements in InstitutionLogin.jsx

--- frontend/src/auth/institution/InstitutionLogin.jsx
+++ frontend/src/auth/institution/InstitutionLogin.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "../../Styles/loginForm.css";
 import { useNavigate } from "react-router-dom";
